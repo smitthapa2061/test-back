5b471bd420710f4e1e43c8d639467e7ba71e9d24
ADMIN <smitthapa57@gmail.com>
2025-10-04 15:43:41 +0545

    v2

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ["http://localhost:3001", "http://localhost:1420", "tauri://localhost"],
+  origin: ["http://localhost:3001", "http://localhost:1420", "tauri://localhost","https://scoresync-v1.vercel.app/"],
   credentials: true,               // allow sending cookies
   methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
